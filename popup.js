e56a8a19a49bd36f7d6eb5cbd2108ab8c7ba3116
amoghjain1988 <amoghjain@hotmail.com>
2023-12-13 18:01:38 -0500

    button works

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function () {
+        alert('Button clicked!');
+        // Add any logic you want to be executed when the button is clicked
+    });
+});
